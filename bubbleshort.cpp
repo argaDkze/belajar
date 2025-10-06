06765371b9dad84f74b5e40d4458b2d29c95e239
argaDkze <muhammadargasetiawan1607@gmail.com>
2025-10-06 15:09:29 +0700

    Add files via upload

--- bubbleshort.cpp
+++ bubbleshort.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int main() 
 {
     int arr[] = {5,3,4,1,2};
     int n = sizeof(arr) / sizeof (arr[0]);
 
     cout << "Array sebelum diurutkan: ";
-for (int i = 0; i < n; i++)
-{
+    for (int i = 0; i < n; i++)
+    {
     cout << arr[i] << " ";
-}
-cout << endl;
+    }
+    cout << endl;
 
-for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n-i-1; i++)
         {
