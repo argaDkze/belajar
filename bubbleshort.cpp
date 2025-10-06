4b5cd3cb09dbafb0c97ccdda61af39dbf5222088
argaDkze <muhammadargasetiawan1607@gmail.com>
2025-10-06 15:21:48 +0700

    Add files via upload

--- bubbleshort.cpp
+++ bubbleshort.cpp
@@ -15,7 +15,7 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n-i-1; i++)
+        for (int j = 0; j < n-i-1; j++)
         {
             if (arr[j] > arr [j + 1])
             {
